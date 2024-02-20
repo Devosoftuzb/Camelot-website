1d847a1eb57302c2a6b859c8c90c6e3e63debd5b
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2024-02-20 17:29:35 +0500

    header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <Header/>
   <main>
     <router-view></router-view>
   </main>
+
 </template>
 
 <script setup>
 
+import Header from './components/Header.vue';
 </script>
 
 <style lang="scss" scoped></style>
