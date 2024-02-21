45ec83f859c0c898d85c05b28a106fc9f47a9d44
jurayevcoder <jurayevcoder@gmail.com>
2024-02-21 14:33:09 +0500

    section 1..3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,179 @@
   <main>
     <section>
       <div class="container mx-auto px-5">
-        <h1>Hello world</h1>
+        <div class="sm:mt-24 mt-10 flex items-center justify-between gap-20">
+          <div class="2xl:w-[800px] lg:w-[600px]">
+            <h1 class="font-bold 2xl:text-[40px] md:text-[35px] text-[24px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut maxime molestiae dolorum quod quo qui, commodi
+              reiciendis
+            </h1>
+            <p class="mt-2 sm:text-[20px] text-[18px] sm:w-full w-[70%] font-medium">
+              * Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit
+            </p>
+
+            <div class="lg:hidden block">
+            <div class="relative 2xl:w-full lg:w-[430px] flex sm:justify-end justify-center lg:h-0 md:h-96 h-60 md:mt-28 md:mb-20 my-10">
+              <img
+                class="absolute z-[-10] 2xl:w-[300px] 2xl:h-[300px] md:w-[250px] md:h-[250px] w-[150px] h-[150px] sm:border-[20px] top-0 left-10 md:left-36 md:top-[-50px] border-[15px] rounded-tl-full rounded-tr-full border-pink-500"
+                src="https://img.freepik.com/free-photo/book-composition-with-open-book_23-2147690555.jpg" alt="">
+              <img
+                class="absolute 2xl:w-[300px] 2xl:h-[300px] md:w-[250px] md:h-[250px] w-[150px] h-[150px] sm:rounded-[60px] rounded-[40px] md:right-36 right-14 top-14 sm:border-[20px] border-[15px] border-lime-400"
+                src="https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip"
+                alt="">
+              <img
+                class="absolute 2xl:w-[300px] 2xl:h-[300px] md:w-[250px] md:h-[250px] w-[150px] h-[150px] rounded-full sm:border-[20px] md:left-36 md:top-36 left-10 top-28 border-[15px] border-blue-400"
+                src="https://st2.depositphotos.com/2769299/7314/i/450/depositphotos_73146775-stock-photo-a-stack-of-books-on.jpg"
+                alt="">
+            </div>
+          </div>
+
+            <div class="grid sm:grid-cols-2 grid-cols-1 gap-5 mt-10">
+              <div class="flex items-center gap-3">
+                <i class='bx bx-badge-check text-[50px] text-blue-400'></i>
+                <p class="2xl:text-[20px] lg:text-[18px] text-[14px] font-medium">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit</p>
+              </div>
+              <div class="flex items-center gap-3">
+                <i class='bx bx-swim text-[50px] text-blue-400'></i>
+                <p class="2xl:text-[20px] lg:text-[18px] text-[14px] font-medium">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit</p>
+              </div>
+              <div class="flex items-center gap-3">
+                <i class='bx bx-restaurant text-[50px] text-blue-400'></i>
+                <p class="2xl:text-[20px] lg:text-[18px] text-[14px] font-medium">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit</p>
+              </div>
+              <div class="flex items-center gap-3">
+                <i class='bx bxs-chat text-[50px] text-blue-400'></i>
+                <p class="2xl:text-[20px] lg:text-[18px] text-[14px] font-medium">Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit</p>
+              </div>
+            </div>
+
+            <div class="mt-10 flex sm:flex-row flex-col items-center sm:gap-10 gap-5">
+              <span class="sm:hidden block font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit</span>
+              <button
+                class="bg-blue-500 sm:w-[250px] w-full text-center sm:p-5 p-3 sm:text-[20px] font-bold text-white rounded-full border border-blue-500 hover:bg-white hover:text-blue-500">Lorem</button>
+              <span class="sm:block hidden font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit</span>
+            </div>
+          </div>
+
+          <div class="lg:block hidden">
+            <div class="relative 2xl:w-full lg:w-[430px] flex justify-end">
+              <img
+                class="absolute z-[-10] 2xl:w-[300px] 2xl:h-[300px] lg:w-[250px] lg:h-[250px] 2xl:top-[-150px] 2xl:left-[-220px] top-[-130px] left-0 border-[20px] rounded-tl-full rounded-tr-full border-pink-500"
+                src="https://img.freepik.com/free-photo/book-composition-with-open-book_23-2147690555.jpg" alt="">
+              <img
+                class="2xl:w-[300px] 2xl:h-[300px] lg:w-[250px] lg:h-[250px] rounded-[60px] border-[20px] border-lime-400"
+                src="https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip"
+                alt="">
+              <img
+                class="absolute 2xl:w-[300px] 2xl:h-[300px] lg:w-[250px] lg:h-[250px] 2xl:top-20 2xl:left-[-220px] top-16 left-0 rounded-full border-[20px] border-blue-400"
+                src="https://st2.depositphotos.com/2769299/7314/i/450/depositphotos_73146775-stock-photo-a-stack-of-books-on.jpg"
+                alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container mx-auto px-5">
+        <div class="sm:mt-32 mt-10 flex flex-col gap-10">
+          <h2 class="md:text-[40px] sm:text-[30px] text-[24px] font-bold">Lorem ipsum dolor sit amet consectetur
+            adipisicing elit</h2>
+          <div class="grid sm:grid-cols-2 grid-cols-1 gap-7">
+            <div class="bg-blue-50 p-10 rounded-3xl flex flex-col gap-y-4">
+              <i class='bx bx-globe sm:text-[50px] text-[35px] text-amber-700'></i>
+              <h3 class="sm:text-[25px] text-[20px] font-bold text-gray-900">Lorem karochi</h3>
+              <p class="sm:text-[18px] text-[14px] font-medium text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit
+              </p>
+            </div>
+
+            <div class="bg-blue-50 p-10 rounded-3xl flex flex-col gap-y-4">
+              <i class='bx bx-globe sm:text-[50px] text-[35px] text-amber-700'></i>
+              <h3 class="sm:text-[25px] text-[20px] font-bold text-gray-900">Lorem karochi</h3>
+              <p class="sm:text-[18px] text-[14px] font-medium text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit
+              </p>
+            </div>
+
+            <div class="bg-blue-50 p-10 rounded-3xl flex flex-col gap-y-4">
+              <i class='bx bx-globe sm:text-[50px] text-[35px] text-amber-700'></i>
+              <h3 class="sm:text-[25px] text-[20px] font-bold text-gray-900">Lorem karochi</h3>
+              <p class="sm:text-[18px] text-[14px] font-medium text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit
+              </p>
+            </div>
+
+            <div class="bg-blue-50 p-10 rounded-3xl flex flex-col gap-y-4">
+              <i class='bx bx-globe sm:text-[50px] text-[35px] text-amber-700'></i>
+              <h3 class="sm:text-[25px] text-[20px] font-bold text-gray-900">Lorem karochi</h3>
+              <p class="sm:text-[18px] text-[14px] font-medium text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="sm:mt-20 mt-10 overflow-x-hidden">
+      <div class="container mx-auto p-5">
+        <div class="items" data-v-e33b1926="">
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+          <div class="entry" data-v-e33b1926="">
+            <img class="sm:w-[400px] w-[250px] rounded-2xl"
+              src="https://img.freepik.com/free-photo/successful-happy-business-team_53876-74892.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1708387200&semt=sph"
+              data-v-e33b1926 alt="" />
+          </div>
+        </div>
       </div>
     </section>
   </main>
@@ -15,4 +187,26 @@ import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.items[data-v-e33b1926] {
+  display: flex;
+  gap: 44px;
+  align-items: center;
+  width: -moz-fit-content;
+  width: fit-content;
+  width: -moz-max-content;
+  width: max-content;
+  animation: carouselAnim-e33b1926 60s infinite alternate linear;
+  overflow-x: auto;
+}
+
+@keyframes carouselAnim-e33b1926 {
+  0% {
+    transform: translate(0);
+  }
+
+  100% {
+    transform: translate(calc(-100% + 1500px));
+  }
+}
+</style>
